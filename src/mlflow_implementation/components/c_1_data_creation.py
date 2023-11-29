59801f411dda4c0b2769f4e62df854c706d2e056
Ishwor Subedi <ishworhero21@gmail.com>
2023-11-29 21:16:30 +0545

    added code for data creation

--- src/mlflow_implementation/components/c_1_data_creation.py
+++ src/mlflow_implementation/components/c_1_data_creation.py
@@ -10,15 +10,18 @@ class CreateSyntheticDataset:
 
     def create_synthetic_dataset(self):
         """
+        For Creating dataset using make classification library
+        we can make as much as data as we want
 
         :return:
         """
         try:
-
             X, y = make_classification(n_samples=self.param.n_samples, n_features=self.param.n_features,
                                        n_classes=self.param.n_classes, random_state=self.param.random_state)
 
-            logger.info(f" Dataset Created with provided info: \n")
+            logger.info(f"Dataset Created with provided info: Samples={self.param.n_samples}, "
+                        f"Features={self.param.n_features}, Classes={self.param.n_classes}, "
+                        f"Random State={self.param.random_state}")
 
         except Exception as e:
             logger.exception(f"An error occurred: {str(e)}")

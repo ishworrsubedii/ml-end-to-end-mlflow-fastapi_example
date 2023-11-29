e39d98902bdcc8431251005419e84dd81246d1cc
Ishwor Subedi <ishworhero21@gmail.com>
2023-11-29 21:23:32 +0545

    pipeline for data preprocessing

--- src/mlflow_implementation/pipeline/stage_2_data_preprocessing.py
+++ src/mlflow_implementation/pipeline/stage_2_data_preprocessing.py
@@ -9,16 +9,18 @@ class DataPreprocessPipeline:
 
     def main(self, X, y):
         """
+        Preprocesses the data.
 
-        :param X:
-        :param y:
-        :return:
-
+        :param X: Input features.
+        :param y: Target labels.
+        :return: Processed training and testing data along with the scaler.
         """
-        param = ConfigFileManagement()
-        data_preprocess_param = param.get_data_preprocessing_config()
-        data_preprocess = DataPreprocess(param=data_preprocess_param)
-        X_train, X_test, y_train, y_test = data_preprocess.split_data(X, y)
-        X_train_scaled, X_test_scaled, scaler = data_preprocess.scale_features(X_train,X_test)
+        config_manager = ConfigFileManagement()
+        data_preprocess_param = config_manager.get_data_preprocessing_config()
+
+        data_preprocessor = DataPreprocess(param=data_preprocess_param)
+
+        X_train, X_test, y_train, y_test = data_preprocessor.split_data(X, y)
+        X_train_scaled, X_test_scaled, scaler = data_preprocessor.scale_features(X_train, X_test)
 
         return X_train, X_test, y_train, y_test, X_train_scaled, X_test_scaled, scaler

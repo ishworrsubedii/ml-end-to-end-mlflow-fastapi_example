7b56259111aa26a4574bab6eb3cc822462e0cca7
Ishwor Subedi <ishworhero21@gmail.com>
2023-11-29 21:26:28 +0545

    data utilities code

--- src/mlflow_implementation/utils/utils.py
+++ src/mlflow_implementation/utils/utils.py
@@ -1,28 +1,30 @@
-import os
 from box.exceptions import BoxValueError
 import yaml
-import json
-import joblib
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
-from typing import Any
 from src.mlflow_implementation import logger
 
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """
+    Reads and loads YAML content into a ConfigBox object
 
-    :param path_to_yaml:
-    :return:
+    :param path_to_yaml: Path to the YAML file
+    :return: ConfigBox object with loaded YAML content
     """
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
-            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            logger.info(f"YAML file '{path_to_yaml}' loaded successfully")
+            if content is None:
+                raise ValueError("YAML file is empty")
             return ConfigBox(content)
-    except BoxValueError:
-        raise ValueError("yaml file is empty")
+    except BoxValueError as box_error:
+        raise ValueError(f"BoxValueError: {box_error}")
+    except FileNotFoundError as file_not_found_error:
+        raise FileNotFoundError(f"File not found: {file_not_found_error}")
     except Exception as e:
-        raise e
+        logger.exception(f"An error occurred while reading YAML file '{path_to_yaml}': {str(e)}")
+        raise

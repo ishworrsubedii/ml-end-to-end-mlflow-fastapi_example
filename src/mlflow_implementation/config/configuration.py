5e78ae7bd0b3dcdc18cca59d6c021ba0deff96f5
Ishwor Subedi <ishworhero21@gmail.com>
2023-11-29 21:22:06 +0545

    added code for config management

--- src/mlflow_implementation/config/configuration.py
+++ src/mlflow_implementation/config/configuration.py
@@ -5,37 +5,54 @@ from src.mlflow_implementation.entity.config_entity import (DataPreprocessingCon
 
 
 class ConfigFileManagement:
-    def __init__(self, param_filepath=PARAMS_FILE_PATH,
-                 config_filepath=CONFIG_FILE_PATH):
+    def __init__(self, param_filepath=PARAMS_FILE_PATH, config_filepath=CONFIG_FILE_PATH):
+        """
+        Initialize ConfigFileManagement class.
+
+        :param param_filepath: Filepath for parameters.
+        :param config_filepath: Filepath for configuration.
+        """
         self.config = read_yaml(config_filepath)
         self.param = read_yaml(param_filepath)
 
     def get_data_creation_config(self) -> DataCreationConfig:
+        """
+        Retrieve DataCreationConfig from parameters.
+
+        :return: DataCreationConfig object.
+        """
         param = self.param.data_creation
         data_creation_config = DataCreationConfig(
             n_samples=param.n_samples,
             n_features=param.n_features,
             n_classes=param.n_classes,
             random_state=param.random_state
-
         )
         return data_creation_config
 
     def get_data_preprocessing_config(self) -> DataPreprocessingConfig:
+        """
+        Retrieve DataPreprocessingConfig from parameters.
+
+        :return: DataPreprocessingConfig object.
+        """
         param = self.param.train_test_split
         data_preprocessing_config = DataPreprocessingConfig(
             test_size=param.test_size,
             random_state=param.random_state
-
         )
         return data_preprocessing_config
 
     def get_model_training_config(self) -> ModelTrainingConfig:
+        """
+        Retrieve ModelTrainingConfig from parameters.
+
+        :return: ModelTrainingConfig object.
+        """
         param = self.param.model_train
         model_training_config = ModelTrainingConfig(
             n_estimators=param.n_estimator,
             random_state=param.random_state
-
         )
         return model_training_config
 
